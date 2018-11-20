e953cded0652700ceea8f19976d5112aa3ec9c75
Tiago Sampaio <tiago@tiagosampaio.com>
2018-11-20 15:26:14 -0200

    Some changes.

--- Model/Source/WeightType.php
+++ Model/Source/WeightType.php
@@ -1,14 +1,4 @@
 <?php
-/**
- * This source file is subject to the MIT License.
- * It is also available through http://opensource.org/licenses/MIT
- *
- * @category  Frenet
- * @package   LithiumSoftware_Akhilleus
- * @author    LithiumSoftware <contato@lithiumsoftware.com.br>
- * @copyright 2015 Lithium Software
- * @license   http://opensource.org/licenses/MIT MIT
- */
 
 namespace MagedIn\Frenet\Model\Source;
 
@@ -17,20 +7,18 @@ class WeightType
     /**
      * Constants for weight
      */
-    public const WEIGHT_GR = 'gr';
-    public const WEIGHT_KG = 'kg';
+    const WEIGHT_GR = 'gr';
+    const WEIGHT_KG = 'kg';
 
     /**
-     * Get options for weight
      * @SuppressWarnings(PHPMD.UnusedLocalVariable)
      * @return array
      */
     public function toOptionArray()
     {
-        return array(
-            array('value' => self::WEIGHT_GR, 'label' => __('Gramas')),
-            array('value' => self::WEIGHT_KG, 'label' => __('Kilos')),
-        );
+        return [
+            ['value' => self::WEIGHT_GR, 'label' => __('Grams')],
+            ['value' => self::WEIGHT_KG, 'label' => __('Kilos')],
+        ];
     }
-
 }
